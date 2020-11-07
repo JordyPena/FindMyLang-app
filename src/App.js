afa45c28ff1392166ea9796bb549c12ad96d9652
Jordy Pena <jordypenamontes@gmail.com>
2020-11-07 13:14:07 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react'
+
+class App extends PureComponent {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <header>Find My Lang</header>
+      </>
+    )
+  }
+}
+
+export default App
