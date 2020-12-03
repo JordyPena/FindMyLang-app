a3c11c8a8ec2c87992af07393ce38bba9aed1853
Jordy Pena <jordypenamontes@gmail.com>
2020-12-02 22:07:47 -0600

    removed catch

--- src/App.js
+++ src/App.js
@@ -57,9 +57,9 @@ class App extends Component {
           languages: langs,
         });
       })
-      .catch((error) => {
-        console.error({ error });
-      });
+      // .catch((error) => {
+      //   console.error({ error });
+      // });
   }
 
   setLanguage = (language) => {
